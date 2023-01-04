4b626d56e683f2ddceac9153531b161a06e994b6
Arseni Skobelev <arseni.skobelev@ikomm.no>
2023-01-04 10:19:01 +0100

    working on documenting the code for further expansion possibility

--- codebase/auth/routes/index.ts
+++ codebase/auth/routes/index.ts
@@ -2,22 +2,32 @@ import express, { Request, Response } from "express";
 
 const router = express.Router();
 
-//  --------------------  import API controllers  -------------------
 
+//                       import API controllers
 import PingController from "../controllers/ping";
 import UserController from "../controllers/user";
 import AuthController from "../controllers/auth";
 
-//  ----------------------------------------------------------------
 
-//  --------------------  import models  ----------------------------
-
+//                          import models
 import { User, IUser } from "../models/user";
 
-//  ----------------------------------------------------------------
 
-//  --------------------  user routes  ------------------------------
+//                          index route
+router.get('/api/v1/', async (_req, res) => {
+    return res.json({ "success": true, "message": `Haudal Authentication Service v${process.env.VERSION}` });
+});
 
+
+//                          test routes
+router.get("/api/v1/ping", async (_req, res) => {
+    const controller = new PingController();
+    const response = await controller.getMessage();
+    return res.send(response);
+});
+
+
+//                          user routes
 router.post('/api/v1/user', async (req: Request, res: Response) => {
     let controller = new UserController();
     let response = await controller.createUser(req.body.user);
@@ -28,21 +38,12 @@ router.post('/api/v1/user', async (req: Request, res: Response) => {
     }
 })
 
+
+//                        session routes
 router.post('/api/v1/session', async (req: Request, res: Response) => {
     const controller = new AuthController();
     let response = await controller.login(req.body.login_data);
 })
 
-//  ----------------------------------------------------------------
-
-router.get("/api/v1/ping", async (_req, res) => {
-    const controller = new PingController();
-    const response = await controller.getMessage();
-    return res.send(response);
-});
-
-router.get('/api/v1/', async (_req, res) => {
-    return res.json({ "success": true, "message": `Haudal Authentication Service v${process.env.VERSION}` });
-});
 
 export default router;
