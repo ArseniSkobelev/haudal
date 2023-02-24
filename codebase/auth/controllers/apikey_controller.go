6231ea5ce73403242bc85b1c126e7459d445bb9e
Arseni Skobelev <arseni.skobelev0@gmail.com>
2023-02-24 14:08:15 +0100

    Added more logging to the API; removed unused comments; tested docker-compose-dev.yaml for dev purposes

--- codebase/auth/controllers/apikey_controller.go
+++ codebase/auth/controllers/apikey_controller.go
@@ -93,31 +93,3 @@ func VerifyKey(c *fiber.Ctx) error {
 
 	return c.Status(fiber.StatusOK).JSON(data)
 }
-
-// func RefreshToken(c *fiber.Ctx) error {
-// 	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
-// 	defer cancel()
-
-// 	authHeader := c.Get("Authorization")
-
-// 	if authHeader == "" {
-// 		return errors.New("No access_token provided")
-// 	}
-// 	var APIKey models.APIKey
-
-// 	APIKey.RefreshToken = authHeader[7:]
-
-// 	max_age, err := strconv.ParseInt(env.GetEnvValue("ACCESS_MAX_AGE", env.DEV), 10, 64)
-
-// 	if err != nil {
-// 		return c.Status(fiber.StatusBadRequest).JSON(responses.AuthorizationResponse{Status: fiber.StatusBadRequest, Message: "Unable to get access_token max age", IsAuthorized: false, Data: APIKey.RefreshToken})
-// 	}
-
-// 	_, err = apikeyCollection.UpdateOne(ctx, bson.M{"refresh_token": APIKey.RefreshToken}, bson.M{"$set": bson.M{"expires_at": time.Now().Unix() + max_age}})
-
-// 	if err != nil {
-// 		return c.Status(fiber.StatusCreated).JSON(responses.KeyResponse{Status: fiber.StatusCreated, Message: "Unable to find provided refresh_token", IsAuthorized: false, Key: APIKey})
-// 	}
-
-// 	return c.Status(fiber.StatusOK).JSON(responses.KeyResponse{Status: fiber.StatusOK, Message: "access_token refreshed successfully", IsAuthorized: true, Key: APIKey})
-// }
--- codebase/auth/controllers/index_controller.go
+++ codebase/auth/controllers/index_controller.go
@@ -3,11 +3,12 @@ package controllers
 import (
 	"log"
 
+	"github.com/ArseniSkobelev/haudal/internal/env"
 	"github.com/ArseniSkobelev/haudal/responses"
 	"github.com/gofiber/fiber/v2"
 )
 
 func Index(c *fiber.Ctx) error {
-	log.Println("[DEBUG] I am hit :)")
+	log.Printf("%v Index enpoint hit", env.GetEnvValue("LOG_PREFIX", env.PRODUCTION))
 	return c.Status(fiber.StatusOK).JSON(responses.IndexResponse{Status: fiber.StatusOK, Message: "Hello :)"})
 }
